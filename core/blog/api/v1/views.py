535bb6a1c7981637ffbe206191f05391e1699d23
Reza-Rezvani <133748212+Reza-Rezvani@users.noreply.github.com>
2023-12-06 17:34:02 +0330

    Change Views
    Change APIView to GenericViews and Mixin

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,8 +6,8 @@ from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
-
-
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import mixins
 
 """@api_view(["GET","POST"])
 @permission_classes([IsAuthenticated])
@@ -20,29 +20,10 @@ def postList(request):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)"""
-
-
-class PostList(APIView):
-    """getting a list of posts and creating a new posts"""
-    permission_classes = [IsAuthenticated]
-    serializer_class = PostSerializer
-
-    def get(self, request):
-        """retrieving  a list of posts"""
-        posts = Post.objects.filter(status=True)
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        """creating a post with provided data"""
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
         return Response(serializer.data)
 
 
-"""@api_view(["GET","PUT","DELETE"])
+@api_view(["GET","PUT","DELETE"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def postDetail(request, id):
     post = get_object_or_404(Post,pk=id,status=True)
@@ -60,7 +41,36 @@ def postDetail(request, id):
     
 
 
-class PostDetail(APIView):
+'''class PostList(APIView):
+    """getting a list of posts and creating a new posts"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get(self, request):
+        """retrieving  a list of posts"""
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        """creating a post with provided data"""
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+'''
+
+class PostList(ListCreateAPIView):
+    """getting a list of posts and creating a new posts"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+
+
+
+
+'''class PostDetail(APIView):
     """ getting detail of the post and edit plus removing it """
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
@@ -86,4 +96,11 @@ class PostDetail(APIView):
         post = get_object_or_404(Post,pk=id,status=True)
         post.delete()
         return Response({"detail":"Item removed successfully"}, status=status.HTTP_204_NO_CONTENT)
+'''
 
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    """ getting detail of the post and edit plus removing it """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
