b5fb0bc4c74b48d1cf48f2bef4e3fd9dfa4420df
Reza-Rezvani <133748212+Reza-Rezvani@users.noreply.github.com>
2024-01-03 17:39:20 +0330

    load testing
    load testing with locust in docker
    create tasks about it

---
+++ core/locust/locustfile.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, task, between
+
+
+class QuickStartUser(HttpUser):
+
+    def on_start(self):
+        response = self.client.post("/accounts/api/v2/jwt/create/", data={
+            "email":"admin@admin.com", "password":"12345"}).json()
+        
+        self.client.headers = {'authorization': f"Bearer {response.get('access', None)}"}
+
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
+
+
+    @task
+    def post_category(self):
+        self.client.get("/blog/api/v1/category/")
