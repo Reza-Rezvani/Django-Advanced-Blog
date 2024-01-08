06e84339793952e3bc8b002c20e4e4ed9e6cfc68
Reza-Rezvani <133748212+Reza-Rezvani@users.noreply.github.com>
2024-01-08 11:34:50 +0330

    background process
    setup redis server in docker-compose
    install and used celery
    add worker to docker-compose
    created task and task schedule
    or use beat

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
+from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("send-email/", views.send_email, name="send-email"),
     # path('api/v1/', include('accounts.api.v1.urls')),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
---
+++ core/core/celery.py
@@ -0,0 +1,24 @@
+import os
+
+from celery import Celery
+from accounts.tasks import sendEmail
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+app = Celery("core")
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, sendEmail.s(), name="send email every 10 seconds")
