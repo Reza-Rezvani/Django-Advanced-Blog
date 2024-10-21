fb4a3768a311403afbf67554c066e4093ac1b032
Reza-Rezvani <133748212+Reza-Rezvani@users.noreply.github.com>
2024-10-21 16:19:10 +0330

    Change blog model
    Change blog model

--- core/blog/models.py
+++ core/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 #getting get user model object
-User = get_user_model()
+#User = get_user_model()
 
 
 class Post(models.Model):
@@ -13,7 +13,7 @@ class Post(models.Model):
     this is a class to define posts for blog app
     """
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey("accounts.Profile", on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=250)
     content = models.TextField()
